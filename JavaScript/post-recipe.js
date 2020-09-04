a11d3fedd521d532d6efe42cb6e514c796db5946
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-09-04 11:21:53 -0300

    Função de criação de receitas

---
+++ JavaScript/post-recipe.js
@@ -0,0 +1,47 @@
+function postRecipe(){
+    let name = document.getElementById('nomeReceita').value;
+    let time = document.getElementById('tempo').value; 
+    let portions = document.getElementById('porcoes').value;
+    let category = document.getElementById('categorias').value;
+    let images = [
+        "https://img.imageboss.me/consul/cdn/animation:true/wp-content/uploads/2013/07/featured-image-6356.jpg",
+        "https://i.pinimg.com/736x/e6/e4/5f/e6e45f617265dd150d824bf1299cb850.jpg"
+    ];
+    let ingredients = [
+        "1kg de batata",
+        "1kg de alface",
+        "1kg de cenoura"
+    ];
+    let steps = [
+        "Mistura tudo",
+        "Mistura tudo",
+        "Mistura tudo"
+    ]
+
+    let aux = time.split(':');
+    time = (Number(aux[0]) * 60) + Number(aux[1]);
+
+    let fullRecipe = {
+        name,
+        time,
+        portions,
+        category,
+        images,
+        ingredients,
+        steps
+    }
+
+    fetch('https://backend-json-server.herokuapp.com/recipes', {
+        method: 'post',
+        body: JSON.stringify(fullRecipe),
+        headers: {'Content-type': 'application/json'}
+    })
+        .then(() => {
+            let alertValue = confirm('Receita Criada com sucesso! \nDeseja voltar a página inicial?');
+
+            if(alertValue === true){
+                window.location.href = "../../index.html";
+            }
+        })
+        .catch(err => alert(err))
+}
