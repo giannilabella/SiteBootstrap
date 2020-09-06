d635d6c71675bc05c7c5305f507e6bf595ba59b9
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-09-06 15:15:13 -0300

    Mudança no sistema de temas
    Mudança no css(layout) do botão de temas e agora o botão já vem automaticamente ativo se o tema escolhido fo o escuro.

--- JavaScript/theme.js
+++ JavaScript/theme.js
@@ -3,17 +3,23 @@
     let theme = localStorage.theme || "light";
 
     linkTheme.setAttribute('href', `Styles/${theme}-theme.css`);
+
+    let chkBox = document.getElementById('check');
+
+    if(theme === "dark"){
+        chkBox.checked = true;
+    }
 })();
 
-function atualiza(){
+function themeUpdate(){
     theme = localStorage.theme || "light";
-
+    
     if(theme === "light"){
         localStorage.setItem('theme', 'dark');
     }else{
         localStorage.setItem('theme', 'light');
     }
-
+    
     let linkTheme = document.getElementById('theme-css');
     theme = localStorage.theme;
 
