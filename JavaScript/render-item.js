b7c30a1d850bf76ced47372b47f370fcccac991d
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-09-12 21:10:20 -0300

    Título da página dinâmico

--- JavaScript/render-item.js
+++ JavaScript/render-item.js
@@ -47,6 +47,7 @@ id = window.location.search.substring(4);
                 }
 
                 document.getElementById('recipeCategory').style.background = color;
+                document.getElementById('page-title').innerText = `${name} | CookBook`
 
                 var passos = document.getElementById('prepareSteps');
                 var ingredientes = document.getElementById('prepareIngredients');
